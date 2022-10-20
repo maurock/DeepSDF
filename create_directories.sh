d6b7d7b42c8cb482d6c41921abcff7503c9a2534
maurock <maurocomi92@gmail.com>
2022-10-20 12:56:03 +0100

    changed objects compression

--- create_directories.sh
+++ create_directories.sh
@@ -9,5 +9,5 @@ touch results/runs/__init__.py
 # Get objects data
 mkdir -p data/
 cd data/
-wget "https://uob-my.sharepoint.com/:u:/g/personal/ri21540_bristol_ac_uk/EWC1KJPvUIVOv14XcnhB7NsBvw07CmEIFVDhseyQSiiXBA?e=daft6H" -O objects.zip
-unzip objects.zip -d data/
+wget "https://uob-my.sharepoint.com/:u:/g/personal/ri21540_bristol_ac_uk/EdigsfvJTn9IkA7QvmepbLYBcqoFXTdmkKPi0ox_yYNzpg?e=3XTBOC" -O objects.tgz
+tar -xvf objects.tgz
