13a9565200996d5e6f35e99ecdd83735772eb205
maurock <maurocomi92@gmail.com>
2022-10-20 12:49:12 +0100

    objects.zip added

--- create_directories.sh
+++ create_directories.sh
@@ -9,5 +9,5 @@ touch results/runs/__init__.py
 # Get objects data
 mkdir -p data/
 cd data/
-wget "https://uob-my.sharepoint.com/:u:/g/personal/ri21540_bristol_ac_uk/EWC1KJPvUIVOv14XcnhB7NsBxwq9lmeCe9uVHbGWK0zGZA?e=kfbiRm" -O objects.zip
+wget "https://uob-my.sharepoint.com/:u:/g/personal/ri21540_bristol_ac_uk/EWC1KJPvUIVOv14XcnhB7NsBvw07CmEIFVDhseyQSiiXBA?e=daft6H" -O objects.zip
 unzip objects.zip -d data/
