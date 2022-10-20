8fdfdde1f98344d4769bc7aa6a9e536b3817da81
maurock <maurocomi92@gmail.com>
2022-10-20 13:12:06 +0100

    removed download data

--- create_directories.sh
+++ create_directories.sh
@@ -5,9 +5,3 @@ mkdir -p results/runs
 
 # Add files __init__.py
 touch results/runs/__init__.py
-
-# Get objects data
-mkdir -p data/
-cd data/
-wget "https://uob-my.sharepoint.com/:u:/g/personal/ri21540_bristol_ac_uk/EdigsfvJTn9IkA7QvmepbLYBcqoFXTdmkKPi0ox_yYNzpg?e=3XTBOC" -O objects.tgz
-tar -xvf objects.tgz
