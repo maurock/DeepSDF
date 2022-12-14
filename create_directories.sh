dfeee18d0721f9effe9fec3ca1c5a6a7f7317c50
maurock <maurocomi92@gmail.com>
2022-12-14 10:09:37 +0000

    Edit create_directories to add runs

---
+++ create_directories.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Create directories if they do not exist
+mkdir -p results/runs_sdf
+mkdir -p results/runs_touch
+
+# Add files __init__.py
+touch results/runs_sdf/__init__.py
+touch results/runs_touch/__init__.py
