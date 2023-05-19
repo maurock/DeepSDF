d1ea872beae9862fd31c101350f81977fd571ed2
maurock <maurocomi92@gmail.com>
2023-05-19 15:55:52 +0100

    Minor edit

--- create_directories.sh
+++ create_directories.sh
@@ -5,6 +5,7 @@ mkdir -p results/runs_sdf
 mkdir -p results/runs_touch
 mkdir -p results/runs_touch_sdf
 mkdir -p data/objects
+mkdir -p data/ShapeNetCoreV2urdf
 
 
 # Add files __init__.py
@@ -12,4 +13,5 @@ touch results/runs_sdf/__init__.py
 touch results/runs_touch/__init__.py
 touch results/runs_touch_sdf/__init__.py
 touch data/objects/__init__.py
+touch data/ShapeNetCoreV2urdf/__init__.py
 touch examples/__init__.py
